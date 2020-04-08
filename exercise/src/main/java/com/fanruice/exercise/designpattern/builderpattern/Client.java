2847a70ab45e5531a534abb1e1d948018bdd3f8a
Ruice fan <fanruice187@163.com>
2020-04-08 23:37:39 +0800

    建造者模式优化

--- exercise/src/main/java/com/fanruice/exercise/designpattern/builderpattern/Client.java
+++ exercise/src/main/java/com/fanruice/exercise/designpattern/builderpattern/Client.java
@@ -11,8 +11,12 @@ public class Client {
     public static void main(String[] args) {
 
         Director director = new Director(new MobikeBuilder());
+        //两种创建的顺序不一样
         Bike bike = director.construct();
         System.out.println(JSON.toJSON(bike));
+        Bike bike1 = director.construct1();
+        System.out.println(JSON.toJSON(bike1));
+
     }
 
 }
