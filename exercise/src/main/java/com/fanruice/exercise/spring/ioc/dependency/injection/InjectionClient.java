8bd0e121c2d8db6ac62a2aeef1d3a1c6eb2463aa
Ruice fan <fanruice187@163.com>
2020-05-06 23:35:40 +0800

    ApplicationContext 和 BeanFactory 之间的区别

--- exercise/src/main/java/com/fanruice/exercise/spring/ioc/dependency/injection/InjectionClient.java
+++ exercise/src/main/java/com/fanruice/exercise/spring/ioc/dependency/injection/InjectionClient.java
@@ -3,6 +3,7 @@ package com.fanruice.exercise.spring.ioc.dependency.injection;
 import com.fanruice.exercise.spring.ioc.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 依赖注入测试
@@ -11,15 +12,23 @@ public class InjectionClient {
 
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-injection-context.xml");
+
+        // ==== 依赖来源1： 自定义的Bean
         //手动配置
         UserRepository userRepository = (UserRepository) beanFactory.getBean("userRepository");
         System.out.println("userRepository:" + userRepository);
 
         //自动配置（根据类型注入）
-        UserRepository userRepositoryAuto = (UserRepository) beanFactory.getBean("userRepositoryAuto", UserRepository.class);
+        UserRepository userRepositoryAuto = beanFactory.getBean("userRepositoryAuto", UserRepository.class);
         System.out.println("userRepositoryAuto:" + userRepositoryAuto);
 
+        // ==== 依赖来源2： 内建的依赖 （如：beanFactory）
         //返回结果为false   依赖注入
+        //ConfigurableApplicationContext <- ApplicationContext <- BeanFactory
+        //
+        //BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-injection-context.xml");
+        // 真正用到的BeanFactory 是ConfigurableApplicationContext#getBeanFactory（）方法所得到的的BeanFactory
+        // 最根源 取的是   AbstractRefreshableApplicationContext 的 一个 DefaultListableBeanFactory类型的属性beanFactory
         System.out.println(userRepositoryAuto.getBeanFactory() == beanFactory);
 
         //依赖查找（会报错）
@@ -32,5 +41,9 @@ public class InjectionClient {
         System.out.println(userRepositoryAuto.getApplicationContextObjectFactory().getObject() == beanFactory);
 
         //最后  我们会发现，依赖注入和依赖查找的来源并不是同一个
+
+        // ==== 依赖来源3： 容器内建的Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println("获取 Environment 类型的Bean：" + environment);
     }
 }
